712e1f3ec99fa42c9c40052cdb89b6fb6fc7caef
Ananya Pandey <117971156+Ananya10x@users.noreply.github.com>
2025-11-23 21:24:18 +0530

    Add scripts.js

---
+++ scripts.js
@@ -0,0 +1,8 @@
+
+        // JavaScript for dynamic features (example)
+        document.querySelectorAll('.feature-card').forEach(card => {
+            card.addEventListener('click', () => {
+                alert('Feature details will be displayed here.');
+            });
+        });
+    
